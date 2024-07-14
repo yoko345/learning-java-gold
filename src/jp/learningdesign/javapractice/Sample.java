151fa68ca333d11336ce1a42b6544c1f07a1fdce
yoko345 <spblackhole89.gmail.com>
2024-07-14 13:03:46 +0900

    first commit

---
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -0,0 +1,21 @@
+package jp.learningdesign.javapractice;
+
+/**
+ * Javadoc形式
+ * @author yokota
+ * @copyright
+ * @param args
+ */
+
+/*
+ * Sampleは、"Hello Eclipse!"と"こんにちは、エクリプス"を表示するプログラムです。
+ */
+public class Sample {
+
+	public static void main(String[] args) {
+		// 次の行で、コンソールに文字列を出力します。
+		System.out.println("Hello Eclipse!");
+		System.out.println("こんにちは、エクリプス");
+	}
+
+}

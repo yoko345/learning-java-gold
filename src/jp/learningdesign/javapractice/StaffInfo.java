c67e153d6539e3a653d804e11a8bef36d0bb56a3
yoko345 <spblackhole89@gmail.com>
2024-07-14 15:10:04 +0900

    新しいメソッドを追加する

--- src/jp/learningdesign/javapractice/StaffInfo.java
+++ src/jp/learningdesign/javapractice/StaffInfo.java
@@ -4,13 +4,20 @@ class Staff {
 	String name;
 	int staffId;
 	String email;
+	
+	public void sayHello() {
+		System.out.println("Hello! " + this.name);
+	}
 }
 
 public class StaffInfo {
 	public static void main(String[] args) {
 		Staff yamada = new Staff();
 		yamada.name = "Taro Yamada";
+//		yamada.staffId = "aaa"; # エラーになる
 		
-		System.out.println(yamada.name);
+		yamada.sayHello();
+		
+//		System.out.println(yamada.name);
 	}
 }

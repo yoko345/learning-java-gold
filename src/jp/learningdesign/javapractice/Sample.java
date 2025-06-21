320e7faa0258ef548e0018bac26b959956cc21fc
yoko345 <spblackhole89@gmail.com>
2025-06-21 12:50:04 +0900

    Runnableインタフェースでの実装

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,9 +1,15 @@
 package jp.learningdesign.javapractice;
 
-
 public class Sample {
     public static void main(String[] args) {
-        Thread t = new SampleThread();
+        // Thread t = new SampleThread();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("sub");
+            }
+        });
+
         t.start();
 
         System.out.println("main");

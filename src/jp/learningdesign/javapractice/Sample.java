1706e5d4e03d9e381bc3f3d4dc83a7dbd5e0013e
yoko345 <spblackhole89@gmail.com>
2025-06-21 12:55:08 +0900

    ラムダ式での表現

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -3,12 +3,13 @@ package jp.learningdesign.javapractice;
 public class Sample {
     public static void main(String[] args) {
         // Thread t = new SampleThread();
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("sub");
-            }
-        });
+        // Thread t = new Thread(new Runnable() {
+        // @Override
+        // public void run() {
+        // System.out.println("sub");
+        // }
+        // });
+        Thread t = new Thread(() -> System.out.println("sub"));
 
         t.start();
 

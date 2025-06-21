4cec62dac082d1e3511e2f6ff054e2d50bf76f9c
yoko345 <spblackhole89@gmail.com>
2025-06-21 17:49:37 +0900

    Callable

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,5 +1,9 @@
 package jp.learningdesign.javapractice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -7,16 +11,34 @@ import java.util.concurrent.Future;
 public class Sample {
     public static void main(String[] args) throws Exception {
         ExecutorService exec = Executors.newSingleThreadExecutor();
-        Future<String> future = exec.submit(() -> {
-            try {
-                System.out.println("start");
-                Thread.sleep(2000);
-                System.out.println("end");
-            } catch (InterruptedException e) {
-                throw new RuntimeException();
-            }
-        }, "終了しました");
 
-        System.out.println(future.get());
+        Callable<Boolean> task = new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return new Random().nextInt() % 2 == 0;
+            }
+        };
+
+        List<Future<Boolean>> futures = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            futures.add(exec.submit(task));
+        }
+
+        List<Future<Boolean>> futures = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            futures.add(exec.submit(() -> new Random().nextInt() % 2 == 0));
+        }
+
+        int total = 0;
+        for (Future<Boolean> future : futures) {
+            Boolean result = future.get();
+            System.out.println(result);
+
+            if (result) {
+                total++;
+            }
+        }
+
+        System.out.println("偶数判定の合計: " + total + "個");
     }
 }

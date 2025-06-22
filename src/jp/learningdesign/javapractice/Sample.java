8b9150169095e9d9911705756273db0ddb47a12c
yoko345 <spblackhole89@gmail.com>
2025-06-22 10:24:50 +0900

    flatMap

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        Optional<String> sample = Optional.empty();
-        sample.ifPresentOrElse(
-            str -> System.out.println(str),
-            () -> System.out.println("empty")
-        );
+        Optional<String> sample = Optional.of("sample");
+        Optional<String> result = sample.flatMap(str -> test(str));
+        System.out.println(result.get()); // SAMPLE
+    }
+
+    private static Optional<String> test(String str) {
+        return Optional.of(str.toUpperCase());
     }
 }

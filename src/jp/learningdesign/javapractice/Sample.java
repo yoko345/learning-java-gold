981d0c2890dd412175104d2a97565bda320a2223
yoko345 <spblackhole89@gmail.com>
2025-06-22 09:04:41 +0900

    orEleseThrow

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,42 +1,10 @@
 package jp.learningdesign.javapractice;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Optional;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        ExecutorService exec = Executors.newSingleThreadExecutor();
-        List<Future<Boolean>> futures = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            futures.add(exec.submit(() -> {
-                if (new Random().nextInt() % 2 != 0) {
-                    throw new Exception("奇数です");
-                }
-                return true;
-            }));
-        }
-
-        int total = 0;
-        for (Future<Boolean> future : futures) {
-            Boolean result = false;
-            try {
-                result = future.get();
-                System.out.println(result);
-            } catch (ExecutionException e) {
-                System.out.println(e.getMessage());
-            }
-
-            if (result) {
-                total++;
-            }
-        }
-
-        System.out.println("偶数判定の合計: " + total + "個");
+        Optional<String> sample = Optional.empty();
+        System.out.println(sample.orElseThrow(() -> new Exception()));
     }
 }

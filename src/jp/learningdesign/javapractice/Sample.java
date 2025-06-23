07a613ba3fc61f066ee51d0e563cd01cc45c7079
yoko345 <spblackhole89@gmail.com>
2025-06-23 22:05:20 +0900

    findFirst, findAny

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,19 +1,21 @@
 package jp.learningdesign.javapractice;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        List<Value> list = Arrays.asList(
-            new Value("A"),
-            new Value("B"),
-            new Value("C"),
-            new Value("A")
-        );
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        long size = list.stream().distinct().count();
-        System.out.println(size);
+        for (int i = 0; i < 5; i++) {
+            list.parallelStream().findAny().ifPresent(System.out::print);
+            System.out.print(", ");
+            list.parallelStream().findFirst().ifPresent(System.out::println);
+        }
+
+        list.parallelStream().filter(x -> {
+            System.out.println(x + " : " + (x % 2 == 0));
+            return x % 2 == 0;
+        }).findFirst().ifPresent(System.out::println);
     }
 
 }

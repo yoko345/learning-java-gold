076873e098f7b118907b66fdbbfbe620c5b5119d
yoko345 <spblackhole89@gmail.com>
2025-06-24 21:16:52 +0900

    max

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -2,16 +2,18 @@ package jp.learningdesign.javapractice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
         List<Integer> list = Arrays.asList(5, 8, 2, 9, 3, 1, 10, 7, 6, 4);
-        list.stream().sorted((a, b) -> {
+        Optional<Integer> result = list.stream().max((a, b) -> {
             if (a < b)
                 return -1;
             if (a > b)
                 return 1;
             return 0;
-        }).forEach(val -> System.out.printf("%d, ", val));
+        });
+        result.ifPresent(System.out::println); // 10
     }
 }

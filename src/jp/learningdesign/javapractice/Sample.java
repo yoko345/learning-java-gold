5bdbec49ad470a0e34163ed8363ec76f803c2a33
yoko345 <spblackhole89@gmail.com>
2025-06-27 21:58:32 +0900

    peek

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        List<String> list = Arrays.asList("a", "b", "c", "d", "e");
-        // String result = list.stream().collect(new SampleCollector());
-        String result = list.parallelStream().collect(new SampleCollector());
-        System.out.println(result); // A, B, C, D, E
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.stream()
+            .map(x -> x * 3)
+            .peek(x -> System.out.println("デバッグ : " + x))
+            .filter(x -> x % 2 == 0)
+            .forEach(x -> System.out.println("結果 : " + x));
     }
 }

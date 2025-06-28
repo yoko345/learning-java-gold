3b029342f2c37153a42aac4abb82047e3ce67bd2
yoko345 <spblackhole89@gmail.com>
2025-06-28 11:10:28 +0900

    list, listFiles

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,15 +1,20 @@
 package jp.learningdesign.javapractice;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        list.stream()
-            .map(x -> x * 3)
-            .peek(x -> System.out.println("デバッグ : " + x))
-            .filter(x -> x % 2 == 0)
-            .forEach(x -> System.out.println("結果 : " + x));
+        File rootDir = new File(".");
+        File[] files = rootDir.listFiles();
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+        System.out.println("");
+
+        String[] fileList = rootDir.list();
+        for (String file : fileList) {
+            System.out.println(file);
+        }
     }
 }

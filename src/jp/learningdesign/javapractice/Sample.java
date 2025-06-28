fdeae6d2350f8fff1362a6181e754deea40e0ecd
yoko345 <spblackhole89@gmail.com>
2025-06-28 11:35:28 +0900

    FileFilter

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,20 +1,28 @@
 package jp.learningdesign.javapractice;
 
 import java.io.File;
+import java.io.FileFilter;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        File rootDir = new File(".");
-        File[] files = rootDir.listFiles();
+        File rootDir = new File("./src/jp/learningdesign/javapractice");
+        FileFilter filter = (path) -> {
+            if (path.isDirectory() || path.getName().startsWith("Sample")) {
+                return true;
+            }
+            return false;
+        };
+
+        showTree(rootDir, filter);
+    }
+
+    private static void showTree(File dir, FileFilter filter) {
+        File[] files = dir.listFiles(filter);
         for (File file : files) {
             System.out.println(file);
-        }
-
-        System.out.println("");
-
-        String[] fileList = rootDir.list();
-        for (String file : fileList) {
-            System.out.println(file);
+            if (file.isDirectory()) {
+                showTree(dir, filter);
+            }
         }
     }
 }

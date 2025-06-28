d099c623265a664a33e8384ec09c03c6a41300c1
yoko345 <spblackhole89@gmail.com>
2025-06-28 15:25:00 +0900

    Console

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,16 +1,20 @@
 package jp.learningdesign.javapractice;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Sample {
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String input = br.readLine();
-            System.out.println(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        // String input = br.readLine();
+        // System.out.println(input);
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+        Console console = System.console();
+        String str = console.readLine();
+        System.out.println(str);
     }
 }

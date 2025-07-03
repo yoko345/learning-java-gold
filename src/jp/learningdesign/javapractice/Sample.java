5d4292741f48652fd35554767cefc75b26edc4a3
yoko345 <spblackhole89@gmail.com>
2025-07-03 21:58:30 +0900

    Queue

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,13 +1,16 @@
 package jp.learningdesign.javapractice;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Sample {
     public static void main(String[] args) {
-        Test<?> test = new Test<>("Hello");
-        String msg = (String) test.getValue();
-        // String msg = test.getValue(); // コンパイルエラー
-        // test.setValue("samlpe"); // コンパイルエラー
-        // test.setValue(new Object()); // コンパイルエラー
-        test.setValue(null);
+        Queue<String> queue = new ArrayDeque<>(5);
+        queue.add("A");
+        queue.add("B");
+        queue.add("C");
+        queue.add("D");
+        queue.add("E");
+        queue.stream().forEach(System.out::print); // ABCDE
     }
 }

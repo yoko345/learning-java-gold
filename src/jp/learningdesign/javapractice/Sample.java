9259942a1084d3245a7134f1ef832ac4884dcbb8
yoko345 <spblackhole89@gmail.com>
2025-07-04 21:56:34 +0900

    Map

--- src/jp/learningdesign/javapractice/Sample.java
+++ src/jp/learningdesign/javapractice/Sample.java
@@ -1,17 +1,29 @@
 package jp.learningdesign.javapractice;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Sample {
     public static void main(String[] args) {
-        Set<String> set = new TreeSet<>();
-        set.add("B");
-        set.add("D");
-        set.add("A");
-        set.add("C");
-        set.add("A");
-        set.add("B");
-        set.stream().forEach(System.out::print); // ABCD
+        Map<String, Integer> map = new HashMap<>();
+        map.put("ONE", 1);
+        map.put("TWO", 2);
+        map.put("THREE", 3);
+        map.keySet().stream().forEach(System.out::println);
+
+        System.out.println("");
+
+        map.entrySet().stream().forEach((Map.Entry<String, Integer> entry) -> {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key + ": " + value);
+        });
+
+        System.out.println("");
+
+        Map<String, Integer> mapEntry = Map.ofEntries(Map.entry("FORE", 4), Map.entry("FIVE", 5));
+        for (Map.Entry<String, Integer> entry : mapEntry.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
